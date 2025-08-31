4ff807648bd95bdad6ece617666df27609c3679f
Shayan Hassan <gamingfanatics2115@gmail.com>
2025-08-31 16:30:10 +0500

    Kilometer Into Mete, Foot Into Inches

---
+++ 6_Problem.py
@@ -0,0 +1,15 @@
+# kilometer into meter
+# Foot into Inches 
+# Converter
+
+
+kilo=int(input("Enter Kilometer : "))
+meter=kilo*1000
+
+print(f"Kilometer {kilo} = Meters {meter}")
+
+foot=int(input("Enter Foot : "))
+inches=foot*12
+
+print(f"Feet {foot} = Inches {inches}")
+

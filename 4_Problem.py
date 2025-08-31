60e73ca965c5707affc322eb82b89a124a2c772d
Shayan Hassan <gamingfanatics2115@gmail.com>
2025-08-31 16:28:09 +0500

    Count Words and Characters in Sentence

---
+++ 4_Problem.py
@@ -0,0 +1,11 @@
+# Take a sentence as input and count how many characters and words it contains.
+
+sentence="My name is Shayan Hassan and i am 20 years old"
+count=len(sentence) # incase of charcters count
+
+# incase of words
+
+countt=len(sentence.split())
+
+print(count)
+print(countt)

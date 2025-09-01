7958badd4a6a5c9da65388697a93109432028426
Shayan Hassan <gamingfanatics2115@gmail.com>
2025-09-01 15:49:07 +0500

    Match Case

---
+++ 9_Problem.py
@@ -0,0 +1,13 @@
+def status_case(status):
+    match status:
+        case 200:
+            return "OK"
+        case 404:
+            return "Not Found"
+        case 500:
+            return "Internal Server Error"
+        case _:
+            return "Invalid"
+        
+answer=status_case(200)
+print(answer)
